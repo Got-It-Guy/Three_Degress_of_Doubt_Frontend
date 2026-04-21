4e9262866828fdeffe0b80c3e9ad9ad7904b259e
yohan020 <yohanlee10@naver.com>
2026-04-21 13:41:39 +0900

    feat : 기본 백엔드 url 변경

--- lib/core/config/backend_config.dart
+++ lib/core/config/backend_config.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 class BackendConfig {
   BackendConfig._();
 
@@ -54,14 +52,6 @@ class BackendConfig {
   }
 
   static String _defaultBaseUrl() {
-    if (kIsWeb) {
-      return 'http://localhost:3000';
-    }
-
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://10.0.2.2:3000';
-    }
-
-    return 'http://localhost:3000';
+    return 'https://fraudprevention.onrender.com';
   }
 }
